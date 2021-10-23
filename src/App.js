3ca65783b29cf2b4c5b411aebf9934c447e67346
Shyam Bahety <bahetyshyam@gmail.com>
2021-10-23 19:25:34 +0000

    Upgraded cra and made improvements for it to be mobile friendly

--- src/App.js
+++ src/App.js
@@ -1,33 +1,28 @@
-import React, { Fragment } from 'react';
-import QuoteBox from './QuoteBox';
+import React, { Fragment } from "react";
+import QuoteBox from "./QuoteBox";
 
+const themes = ["black", "grey", "red", "green", "blue"];
 class App extends React.Component {
-
-  // themes = ['black', 'grey', 'red', 'green', 'blue'];
-
-  constructor(props) {
-    super(props);
-    this.themes = ['black', 'grey', 'red', 'green', 'blue'];
-    this.state = {
-      currentTheme: 'black'
-    }
-  }
+  state = {
+    currentTheme: "black",
+  };
 
   handleThemeChange = () => {
-    // const themes = this.themes;
-    // console.log(themes);
-    const randTheme = Math.floor(Math.random() * this.themes.length);
-    this.setState({currentTheme:this.themes[randTheme]})
-  }
+    const randTheme = Math.floor(Math.random() * themes.length);
+    this.setState({ currentTheme: themes[randTheme] });
+  };
 
   render() {
     return (
       <Fragment>
-        <div className={'container' + ' ' + this.state.currentTheme} >
-          <QuoteBox currentTheme={this.state.currentTheme} handleThemeChange={this.handleThemeChange} />
+        <div className={`container ${this.state.currentTheme}`}>
+          <QuoteBox
+            currentTheme={this.state.currentTheme}
+            handleThemeChange={this.handleThemeChange}
+          />
         </div>
       </Fragment>
-    )
+    );
   }
 }
 
--- src/QuoteBox.js
+++ src/QuoteBox.js
@@ -1,56 +1,71 @@
-import React from 'react';
-import Button from './Button';
+import React from "react";
+import TwitterSvg from "./assets/svg/twitter.svg";
 
 class QuoteBox extends React.Component {
+  state = {
+    result: undefined,
+    currentQuote: "",
+    currentAuthor: "",
+  };
 
-    state = {
-        result: undefined,
-        currentQuote: '',
-        currentAuthor: '',
-    }
-
-    componentDidMount() {
-        fetch('https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json')
-            .then(response => response.json())
-            .then(result => this.setState({ result, currentQuote: result.quotes[0].quote, currentAuthor: result.quotes[0].author }));
-    }
-
-    handleNewQuote = (event) => {
-        this.props.handleThemeChange();
-        const randQuote = this.state.result && Math.floor(Math.random() * this.state.result.quotes.length);
+  componentDidMount() {
+    fetch(
+      "https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json"
+    )
+      .then((response) => response.json())
+      .then((result) =>
         this.setState({
-            currentQuote: this.state.result.quotes[randQuote].quote,
-            currentAuthor: this.state.result.quotes[randQuote].author
+          result,
+          currentQuote: result.quotes[0].quote,
+          currentAuthor: result.quotes[0].author,
         })
-    }
+      );
+  }
 
+  handleNewQuote = (event) => {
+    this.props.handleThemeChange();
+    const randQuote =
+      this.state.result &&
+      Math.floor(Math.random() * this.state.result.quotes.length);
+    this.setState({
+      currentQuote: this.state.result.quotes[randQuote].quote,
+      currentAuthor: this.state.result.quotes[randQuote].author,
+    });
+  };
 
+  handleTweet = () => {
+    let tweetUrl = `https://twitter.com/intent/tweet?hashtags=quotes&&text=+${encodeURIComponent(
+      '"' + this.state.currentQuote + '" ' + this.state.currentAuthor
+    )}`;
+    window.open(tweetUrl, "_blank").focus();
+  };
 
-    render() {
+  render() {
+    return (
+      <div className="quote-box">
+        <div className="quote-text">
+          <i className="fas fa-quote-left"></i>
+          <span>{this.state.currentQuote}</span>
+        </div>
+        <div className="quote-author">- {this.state.currentAuthor}</div>
 
-        let endodeText = encodeURIComponent(this.state.currentQuote);
-        let tweetLink = `https://twitter.com/intent/tweet?hashtags=quotes&&text=+${encodeURIComponent('"' + this.state.currentQuote + '" ' + this.state.currentAuthor)}`;
-        console.log(tweetLink);
-        return (
-            <div className="quote-box">
-                <div className="quote-text">
-                    <i className="fas fa-quote-left"></i>
-                    <span>
-                        {this.state.currentQuote}
-                    </span>
-                </div>
-                <div className="quote-author">
-                    - {this.state.currentAuthor}
-                </div>
-
-                <div className="buttons">
-                    <Button currentTheme={this.props.currentTheme} float={"left"}>A</Button>
-                    <Button currentTheme={this.props.currentTheme} float={"left"}>B</Button>
-                    <Button currentTheme={this.props.currentTheme} onClick={this.handleNewQuote} float={"right"} marginRight={0}>New Quote</Button>
-                </div>
-            </div>
-        )
-    }
+        <div className="buttons">
+          <div
+            onClick={this.handleTweet}
+            className={`button button-${this.props.currentTheme}`}
+          >
+            <img src={TwitterSvg} height={20} width={20} alt="Twitter" />
+          </div>
+          <div
+            onClick={this.handleNewQuote}
+            className={`button button-${this.props.currentTheme}`}
+          >
+            New Quote
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default QuoteBox;
